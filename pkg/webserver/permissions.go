1294e89d66d5978327a8ec7b001130ead2a571a1
Gustavo Ferreira <gustavojcoferreira@gmail.com>
2019-08-30 15:45:21 +0100

    Added permissions functionality

---
+++ pkg/webserver/permissions.go
@@ -0,0 +1,48 @@
+package webserver
+
+type Permissions struct {
+	Global  []string
+	Channel map[string][]string
+}
+
+func NewPermissions(globalPermissions []string, channelPermissions map[string][]string) Permissions {
+	if channelPermissions == nil {
+		channelPermissions = make(map[string][]string)
+	}
+	return Permissions{Global: globalPermissions, Channel: channelPermissions}
+}
+
+func (p *Permissions) AddGlobal(userID string) {
+	p.Global = append(p.Global, userID)
+}
+
+func (p *Permissions) AddChannel(channelID string, userID string) {
+	if value, ok := p.Channel[channelID]; ok {
+		value = append(value, userID)
+	} else {
+		p.Channel[channelID] = []string{userID}
+	}
+}
+
+func (p Permissions) ValidateGlobal(userID string) bool {
+	for _, elem := range p.Global {
+		if elem == userID {
+			return true
+		}
+	}
+	return false
+}
+
+func (p Permissions) ValidateChannel(channelID string, userID string) bool {
+
+	if value, ok := p.Channel[channelID]; !ok {
+		return false
+	} else {
+		for _, elem := range value {
+			if elem == userID {
+				return true
+			}
+		}
+		return false
+	}
+}
